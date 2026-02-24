7bdbb48ed500fd5e6811c86159ccb1b699a8d2a7
Noah-ke24 <timgryziec19@gmail.com>
2026-02-24 15:21:49 -0700

    good commit

--- src/models.py
+++ src/models.py
@@ -7,3 +7,27 @@ class Player:
         self._health = health  #magic method that allows us to access the health attribute using player.
                                     #health instead of player._health
         self.completed_tasks = 0
+
+@property
+    def health(self):
+        return self._health
+
+    def take_damage(self, amount):
+        if amount <= 0:
+            return
+        self._health -= amount
+        if self._health < 0:
+            self._health = 0
+
+    def heal(self, amount):
+        if amount <= 0:
+            return
+        self._health += amount
+        if self._health > 100:
+            self._health = 100
+
+    def complete_task(self):
+        self.completed_tasks += 1
+
+    def is_alive(self):
+        return self._health > 0
