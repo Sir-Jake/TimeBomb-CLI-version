026ca9eaae4fdd313eb5c6391a9287a2c8bb533d
Jelagat2640 <sugutasumpta@gmail.com>
2026-02-25 20:08:52 +0300

    moving file

--- src/storage.py
+++ src/storage.py
@@ -45,7 +45,15 @@ def save_tasks(tasks):
 
 def move_file(filename,destination_folder):
     source_file=os.path.join(DATA_DIR,filename)
+    #check does the file exist
     if not os.path.exists(source_file):
         print(f"File {filename} does not exist. Cannot move")
         return
-        
+
+    #creating a destination folder if it missing
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+        #moving the file to the new folder
+    shutil.move(source_file,os.path.join(destination_folder,filename))
+    
+
